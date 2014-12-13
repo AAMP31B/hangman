b2586cbf67b33fb9bb5d4af75f5d1a4e9ef4cabb
AAMP31B <AAMP31B@Gmail.com>
2014-12-13 11:27:35 -0800

    First Commit

---
+++ Hangman/Hangman.cpp
@@ -0,0 +1,13 @@
+/*******************************************************************
+Michael Hodges
+C++ Final - Hangman
+This program will simulate a game of hangman.
+Input: File Input for wordlist / userinput for guesses
+Output: Hangman Visuals, Guessed letters, Used Letters, Tries
+*******************************************************************/
+
+int main()
+{
+	return 0;
+}
+
